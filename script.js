bf3c671999213adc8aa7de4c23a278e4aa95d565
Matt Giannacco <matthew.giannacco@gmail.com>
2025-08-07 14:40:51 -0400

    Mobile-friendly beta

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Script loaded");
+
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+}
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+}
